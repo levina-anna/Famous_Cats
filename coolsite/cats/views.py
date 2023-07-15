fb8e11e3299a5cc00a685d1f1feb8c75643e6c86
n0root <n0root@mail.ru>
2023-07-15 16:33:43 +0300

    added Archive by years

--- coolsite/cats/views.py
+++ coolsite/cats/views.py
@@ -9,3 +9,6 @@ def index(request): # request - это ссылка на класс HttpRequest
 # отображения списка статей по рубрикам
 def categories(request, categoriesid):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{categoriesid}</p>")
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
