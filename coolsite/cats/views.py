f6ba4792b406f7f85fa9c52bdec9dd0db4df42a9
n0root <n0root@mail.ru>
2023-07-15 16:43:39 +0300

    added GET request processing

--- coolsite/cats/views.py
+++ coolsite/cats/views.py
@@ -7,7 +7,10 @@ def index(request): # request - это ссылка на класс HttpRequest
     return HttpResponse("Страница приложения Cats")
 
 # отображения списка статей по рубрикам
-def categories(request, categoriesid):
+def categories(request, categoriesid): # http://127.0.0.1:8000/categories/1/?name=Moosya&type=siberian
+    if(request.GET):
+        print(request.GET) # {'name': ['Moosya'], 'type': ['siberian']}
+
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{categoriesid}</p>")
 
 def archive(request, year):
