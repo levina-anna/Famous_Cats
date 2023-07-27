edce2ffb476ec6a80fab21556aef3712eda1f29f
n0root <n0root@mail.ru>
2023-07-27 20:30:57 +0300

    added function pagination

--- coolsite/cats/views.py
+++ coolsite/cats/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
@@ -11,6 +12,7 @@ from .utils import *
 
 # Класс для главной страницы index.html
 class CatsHome(DataMixin, ListView):
+    paginate_by = 3
     model = Cats
     template_name = 'cats/index.html'
     context_object_name = 'posts'
@@ -26,7 +28,11 @@ class CatsHome(DataMixin, ListView):
 
 # функция представления для страницы "О сайте"
 def about(request):
-    return render(request, 'cats/about.html', {'menu': menu, 'title': 'О сайте'})
+    contact_list = Cats.objects.all()
+    paginator = Paginator(contact_list, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'cats/about.html', {'page_obj': page_obj, 'menu': menu, 'title': 'О сайте'})
 
 
 class AddPage(LoginRequiredMixin, DataMixin, CreateView):
