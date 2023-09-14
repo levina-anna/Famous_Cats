f2bb97debd8668ac5d4b276df48f2ad94210e618
a.levina <a.levina@severnaya.ru>
2023-09-14 19:51:07 +0300

    add styles for github button

--- coolsite/cats/views.py
+++ coolsite/cats/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth import logout, login
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.core.paginator import Paginator
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
