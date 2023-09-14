c32ff91c91a09f28495edaddaa811d476748e4e1
Anna <anna_levina@internet.ru>
2023-09-14 21:10:10 +0300

    update AddPage

--- coolsite/cats/views.py
+++ coolsite/cats/views.py
@@ -47,7 +47,7 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title="Add category")
+        c_def = self.get_user_context(title="Add page")
         return dict(list(context.items()) + list(c_def.items()))
 
 
