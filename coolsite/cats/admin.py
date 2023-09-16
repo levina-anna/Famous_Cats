dd438018acaa7a3849bed54f1ce4e64249d82e30
Anna <anna_levina@internet.ru>
2023-09-16 13:05:06 +0300

    tuning admin panel

--- coolsite/cats/admin.py
+++ coolsite/cats/admin.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
 
 class CatsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_published', 'time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    save_on_top = False
 
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = "Miniature"
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
@@ -21,3 +30,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Cats, CatsAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = "Admin panel about cats"
+admin.site.site_header = "Admin panel about cats"
